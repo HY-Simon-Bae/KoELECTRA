02aa8534c4f9bfe9f132110873de3b329dc757cf
monologg <adieujw@gmail.com>
2020-09-06 18:49:51 +0900

    feat: vocab size 51000

--- pretrain/script/base.sh
+++ pretrain/script/base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 run_pretraining.py --data-dir gs://koelectra-v3 \
+python3 run_pretraining.py --data-dir gs://koelectra-v3-51000 \
                            --model-name koelectra-v3-base \
                            --hparams config/base_config.json
